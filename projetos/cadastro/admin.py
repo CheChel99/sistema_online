899e48a3cdb3064bedec38d186c3b3c343d52ddd
Excel Avançado 2019.6 <39117022019.6@senacrjedu.local>
2024-06-24 21:30:47 -0300

    Cadastro de Marcas

---
+++ projetos/cadastro/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from cadastro.models import Marca
+
+from cadastro.models import Cliente
+
+# Register your models here.
+admin.site.register(Marca)
+ 
+admin.site.register(Cliente)
---
+++ projetos/cadastro/forms.py
@@ -0,0 +1,8 @@
+from django.forms import ModelForm
+
+from .models import Marca
+
+class MarcaForm(ModelForm):
+    class Meta:
+        model = Marca
+        fields = '__all__'
